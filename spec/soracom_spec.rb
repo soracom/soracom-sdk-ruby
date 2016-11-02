e0e8c8df8dbc60b30be0fb68435cdbebcc39cc9d
naofumi-fujii <naofumi.fujii@gmail.com>
2016-11-02 21:16:53 +0900

    fixed specs to current implementaion

--- spec/soracom_spec.rb
+++ spec/soracom_spec.rb
@@ -10,10 +10,10 @@ describe Soracom do
   end
 
   it 'initialize fails with invalid credentials' do
-    expect { Soracom::Client.new(email: 'somebody@example.com', password: 'foobar') }.to raise_error('400 Bad Request')
+    expect { Soracom::Client.new(email: 'somebody@example.com', password: 'foobar') }.to raise_error SystemExit
   end
 
   it 'initialize succeeds with valid credentials' do
-    expect { Soracom::Client.new(email: 'REPLACEME', password: 'REPLACEME') }.to raise_error('400 Bad Request')
+    expect { Soracom::Client.new(email: 'REPLACEME', password: 'REPLACEME') }.to raise_error SystemExit
   end
 end
